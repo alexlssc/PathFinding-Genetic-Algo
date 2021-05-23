c27cb3e14d8ef6751bd86ded1b6deab3d5275b26
Alexandre Lissac <alex36330@gmail.com>
2021-05-23 14:22:55 +0200

    Generation text added

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ import pygame
 import sys
 from indiv import Individual as indi
 
-pygame.init()
-pygame.display.set_caption("Path-Finding Genetic Algorithm")
-
 SIZE = width, height = 800, 800
 BACKGROUND = [8, 126, 139]
 ENTITY_COLOUR = [255, 90, 95]
@@ -18,34 +15,38 @@ GENE_LENGTH = 80
 
 def main():
     global SCREEN
+    pygame.init()
+    pygame.font.init()
+    pygame.display.set_caption("Path-Finding Genetic Algorithm")
     SCREEN = pygame.display.set_mode(SIZE)
     CLOCK = pygame.time.Clock()
-
+    font = pygame.font.Font(pygame.font.get_default_font(), 20)
     # Init population
     pop = [indi(400, 100, GENE_LENGTH, ENTITY_COLOUR, SCREEN)
            for _ in range(NB_POP)]
     for indiv in pop:
         indiv.setRandomGenes()
 
-    allFitness = []
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
-        for _ in range(NB_GEN):
+        for gen in range(NB_GEN):
+            allFitness = []
             for _ in range(GENE_LENGTH):
                 SCREEN.fill(BACKGROUND)
                 CLOCK.tick(FPS)
+                text_surface = font.render(
+                    f'Generation: {gen}', True, (255, 255, 255))
+                SCREEN.blit(text_surface, dest=(0, 0))
                 pygame.draw.circle(SCREEN, ENTITY_COLOUR, ENDPOINT, 20)
                 for indiv in pop:
                     indiv.draw()
                 pygame.display.flip()
-        else:
-            for indiv in pop:
-                allFitness.append(indiv.getFitness())
-            print(allFitness)
+            else:
+                for indiv in pop:
+                    allFitness.append(indiv.getFitness())
+                print(allFitness)
         break
 
 
